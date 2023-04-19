df2d408c0dbc1323e551028a18258f00fa3d3975
ZoZeck <cool.maksoon@gmail.com>
2023-04-19 20:08:24 +0300

    added steam api

--- parser_1.py
+++ parser_1.py
@@ -4,20 +4,20 @@ from selenium.webdriver.chrome.service import Service
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.action_chains import ActionChains
 
+import os
 import time
 import json
+import requests
 
 options = webdriver.ChromeOptions()
-options.add_argument('user-data-dir=C:\\Users\\coolm\\AppData\\Local\\Google\\Chrome\\User Data\\Default')
+options.add_argument(f'user-data-dir={os.path.join(os.environ["LOCALAPPDATA"], "Google/Chrome/User Data/Default")}')
 
 options.add_argument("--start-maximized")
 options.add_argument('--disable-blink-features=AutomationControlled')
 options.add_experimental_option('excludeSwitches', ['enable-automation'])
 options.add_experimental_option('useAutomationExtension', False)
 
-s = Service(executable_path='./User/chromedriver.exe')
-
-driver = webdriver.Chrome(service=s, chrome_options=options)
+driver = webdriver.Chrome(chrome_options=options)
 
 driver.execute_cdp_cmd('Page.addScriptToEvaluateOnNewDocument', {
     'source': '''
@@ -33,38 +33,21 @@ def config():
         return skins_config
 
 def best_idea(config):
-    def steam_price_checker(link_for_steam):
-        try:
-            main_window = driver.current_window_handle
-            steam_link = r'https://steamcommunity.com/market/listings/730/'
-            currect_link = steam_link + link_for_steam
-            driver.switch_to.new_window('tab')
-            driver.get(currect_link)
-            price = driver.find_element(By.XPATH, '//*[@class = "market_listing_price market_listing_price_with_fee"]').text
-            driver.close()
-            driver.switch_to.window(main_window)                
-        finally:
-            return int(price[:price.find(',')])
-
-
     for_replace_front, for_replace_back  = 'background-image: url("https://steamcdn-a.akamaihd.net/apps/730/icons/econ/stickers/', '.png");'
     rarity = ['Well-Worn', 'Field-Tested', 'Battle-Scarred', 'Minimal Wear', 'Factory New']
+    weapon_type = 'AK-47'
+    weapon_name = 'Slate'
     try:
+        prices = dict()
         for quality in rarity:
-            url = [f'https://market.csgo.com/ru/?sort=price&order=asc&search=AK-47%20%7C%20Slate%20({quality})&priceMax=1000000&categories=any_stickers']
+            r = requests.get(f'http://steamcommunity.com/market/priceoverview/?currency=1&appid=730&market_hash_name={weapon_type}%20%7C%20{weapon_name}%20%28{quality}%29')
+            prices[quality] = json.loads(r.content.decode('utf-8'))['lowest_price']
+        for quality in rarity:
+            url = [f'https://market.csgo.com/ru/?sort=price&order=asc&search={weapon_type}%20%7C%20{weapon_name}%20({quality})&priceMax=1000000&categories=any_stickers']
             for link in url:
                 driver.get(link)
-                if r'AK-47%20%7C%20Slate%20(Well-Worn)' in link:
-                    steam_price = int(steam_price_checker(r'AK-47%20%7C%20Slate%20%28Well-Worn%29') / 80 )
-                elif r'AK-47%20%7C%20Slate%20(Field-Tested)' in link:
-                    steam_price = int(steam_price_checker(r'AK-47%20%7C%20Slate%20%28Field-Tested%29') / 80)
-                elif r'AK-47%20%7C%20Slate%20(Battle-Scarred)' in link:
-                    steam_price = int(steam_price_checker(r'AK-47%20%7C%20Slate%20%28Battle-Scarred%29') / 80)
-                elif r'AK-47%20%7C%20Slate%20(Minimal Wear)' in link:
-                    steam_price = int(steam_price_checker(r'AK-47%20%7C%20Slate%20%28Minimal%20Wear%29') / 80)
-                elif r'AK-47%20%7C%20Slate%20(Factory New)' in link:
-                    steam_price = int(steam_price_checker(r'AK-47%20%7C%20Slate%20%28Factory%20New%29') / 80)
-                print(f'Steam price - {steam_price} $')
+                print(f'Steam price - {prices[quality]}')
+                actually_price = float(prices[quality][prices[quality].find('$')+1:])
                 actions = ActionChains(driver)
                 driver.implicitly_wait(20)
                 skins_data = []
@@ -80,14 +63,14 @@ def best_idea(config):
                             if sticker_for_config in config and 'price' in config[sticker_for_config]:
                                 sum = config[sticker_for_config]['price'] + sum
                                 stickers.append(config[sticker_for_config]['name'])
-                            if len(stickers) == 4 and stickers[0] == stickers[1] and stickers[1] == stickers[2] and stickers[2] == stickers[3] and sum > steam_price:
-                                if (((((config[sticker_for_config]['price'] *0.5) + steam_price) - (float(i.text.split()[1]))) *100)/(float(i.text.split()[1]))) >= 30:
+                            if len(stickers) == 4 and stickers[0] == stickers[1] and stickers[1] == stickers[2] and stickers[2] == stickers[3] and sum > actually_price:
+                                if (((((config[sticker_for_config]['price'] *0.5) + actually_price) - (float(i.text.split()[1]))) *100)/(float(i.text.split()[1]))) >= 30:
                                     if i.get_attribute('href') not in skins_data:
                                         skins_data.append(i.get_attribute('href'))
                                         print(f"\n{i.get_attribute('href')}\n{' '.join(i.text.split()[4:])}: {float(i.text.split()[1]):.2f} $")
                                         print(f"{config[sticker_for_config]['name']} - {config[sticker_for_config]['price']:.2f} $")
                                         print(f"Sum of stickers: {sum:.2f} $ ")
-                                        print(f"Процент потенциальной выручки - {(((((config[sticker_for_config]['price'] * 0.5) + steam_price) - (float(i.text.split()[1]))) *100)/(float(i.text.split()[1]))):.2f} %")
+                                        print(f"Процент потенциальной выручки - {(((((config[sticker_for_config]['price'] * 0.5) + actually_price) - (float(i.text.split()[1]))) *100)/(float(i.text.split()[1]))):.2f} %")
 
                     element = item_url[-1]
                     actions.move_to_element(element).perform()
