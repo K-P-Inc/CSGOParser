ef6e245ff9628a96c960c9cc2390afdca35ddfd0
Maksim Prokhorenkov <maksimprokhorenkov@Maksims-MacBook-Air.local>
2024-01-15 20:16:45 +0100

    fix(auth): refactor root.tsx

--- app/app/root.tsx
+++ app/app/root.tsx
@@ -8,12 +8,9 @@ import {
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
-import { useState } from "react";
 import Layout from "./components/layout";
 import stylesheet from "~/tailwind.css";
-import { Navigate } from "react-router-dom";
 import Auth from "./routes/auth";
-import SigninForm from "./components/forms/SigninForm";
 
 export const links: LinksFunction = () => [
   { rel: "stylesheet", href: stylesheet },
@@ -52,23 +49,3 @@ export default function App() {
     </html>
   );
 };
-
-//   return (
-    // <html lang="en">
-    //   <head>
-    //     <meta charSet="utf-8" />
-    //     <meta name="viewport" content="width=device-width, initial-scale=1" />
-    //     <Meta />
-    //     <Links />
-    //   </head>
-      // <body>
-        // <Layout>
-        //   <Outlet />
-        // </Layout>
-        // <ScrollRestoration />
-        // <Scripts />
-        // <LiveReload />
-      // </body>
-//     </html>
-//   );
-// }
