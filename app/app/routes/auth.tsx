1dd375c79a3195724fc06ca4cb0d2a272d6aec25
Maksim Prokhorenkov <maksimprokhorenkov@Maksims-MacBook-Air.local>
2024-01-15 19:37:44 +0100

    fix(auth): refactor auth.tsx

--- app/app/routes/auth.tsx
+++ app/app/routes/auth.tsx
@@ -1,16 +1,10 @@
-
-// import './Auth.css';
-
-import { Outlet, Navigate } from "react-router-dom";
 import SigninForm from "~/components/forms/SigninForm";
-// import SignupForm from "~/components/forms/SignupForm";
 
 export default function Index() {
 
   return (
     <>
     <SigninForm />
-    {/* <SignunForm /> */}
     </>
   )
 }
