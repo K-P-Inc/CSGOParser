924099b0d9fc463270bbe2d41da26b8838b6737c
sprili <slidedc@gmail.com>
2024-06-17 22:04:35 +0200

    tests

--- tests/test_marketsHelpers.py
+++ tests/test_marketsHelpers.py
@@ -38,8 +38,8 @@ class TestData:
 markets = [pytest.param(market_class, id=market_class.DB_ENUM_NAME) for market_class in [
     CSMoneyHelper(),
     MarketCSGOHelper(),
-    SkinbidHelper(),
-    CSFloatHelper(),
+    # SkinbidHelper(),
+    # CSFloatHelper(),
     BitskinsHelper(),
     HaloskinsHelper(),
     DmarketHelper(),
