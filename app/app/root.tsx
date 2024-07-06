b855824756af25be0659e0aca4a8b66e8a057b18
ZoZeck <cool.maksoon@gmail.com>
2024-07-06 10:31:16 +0200

    add: metatag for corp email

--- app/app/root.tsx
+++ app/app/root.tsx
@@ -120,6 +120,7 @@ export default function App() {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="mailru-domain" content="7s8VkUFnpzKSr4oy" />
         <Meta />
         <Links />
       </head>
