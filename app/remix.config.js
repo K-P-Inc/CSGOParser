e7ec9bb8f6e4867222fa12d94769f29c338b2580
sprili <slidedc@gmail.com>
2025-03-12 13:42:03 +0100

    fixed

--- app/remix.config.js
+++ app/remix.config.js
@@ -8,4 +8,5 @@ export default {
   serverDependenciesToBundle:  ['swiper', 'swiper/react', 'swiper/react/swiper-react.js', 'ssr-window','ssr-window/ssr-window.esm.js','dom7'],
   tailwind: true,
   postcss: true,
+  sourcemap: false
 };
